06f6a44212c48dc60758668f98f8cc3ef17dbc52
kotik <mak4alex@gmail.com>
2019-05-11 18:28:53 +0300

    update pika adapters usage, remove channel creation

--- scrapy_rabbitmq/connection.py
+++ scrapy_rabbitmq/connection.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import logging
 
 try:
     import pika
@@ -15,11 +16,11 @@ RABBITMQ_DSN = 'amqp://guest:guest@localhost/'
 def from_settings(settings):
     """ Factory method that returns an instance of channel
 
-        :param str connection_type: This field can be `blocking`
-            `asyncore`, `libev`, `select`, `tornado`, or `twisted`
+        :param str connection_type: This field specifies connection adapter, it can be
+            `blocking`, `select`, `tornado` or `twisted`
 
         See pika documentation for more details:
-            TODO: put pika url regarding connection type
+            https://pika.readthedocs.io/en/stable/modules/adapters/index.html
 
         Parameters is a dictionary that can
         include the following values:
@@ -41,28 +42,28 @@ def from_settings(settings):
 
         :return: Channel object
     """
-
+    
     connection_type = settings.get('RABBITMQ_CONNECTION_TYPE', RABBITMQ_CONNECTION_TYPE)
-    queue_name = settings.get('RABBITMQ_QUEUE_NAME', RABBITMQ_QUEUE_NAME)
     connection_parameters = settings.get('RABBITMQ_CONNECTION_PARAMETERS', RABBITMQ_CONNECTION_PARAMETERS)
     connection_dsn = settings.get('RABBITMQ_DSN', RABBITMQ_DSN)
+    pika_log_level = settings.get('RABBITMQ_PIKA_LOG_LEVEL', logging.WARNING)
 
-    connection_producer = {
-        'blocking': pika.BlockingConnection,
-        'select': pika.SelectConnection,
-        'tornado': pika.TornadoConnection,
-        'twisted': pika.TwistedConnection
-    }[connection_type]
+    logging.getLogger('pika').setLevel(pika_log_level)
+
+    if connection_type == 'blocking':
+        from pika import BlockingConnection as ConnectionAdapter
+    elif connection_type == 'select':
+        from pika import SelectConnection as ConnectionAdapter
+    elif connection_type == 'tornado':
+        from pika.adapters.tornado_connection import TornadoConnection as ConnectionAdapter
+    elif connection_type == 'twisted':
+        from pika.adapters.twisted_connection import TwistedProtocolConnection as ConnectionAdapter
+    else:
+        raise RuntimeError("Adapter '{}' was not found!".format(connection_type))
 
     if connection_dsn:
-        connection = connection_producer(pika.URLParameters(connection_dsn))
+        connection = ConnectionAdapter(pika.connection.URLParameters(connection_dsn))
     else:
-        connection = connection_producer(pika.ConnectionParameters(**connection_parameters))
+        connection = ConnectionAdapter(pika.connection.ConnectionParameters(**connection_parameters))
 
-    channel = connection.channel()
-    channel.queue_declare(queue=queue_name, durable=True)
-
-    return channel
-
-
-
+    return connection
